8bed5d6c7e6227629b67bcbf3c0041dba14d8c4a
kyledowning <kyleshinya@gmail.com>
2026-03-20 15:12:33 -0700

    final project update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # faices
-### Contributors: Charlie Laursen, Kyle Downing
+## Contributors: Charlie Laursen, Kyle Downing
 
 Class project for **Privacy Enhancing Technologies** (CSCI-497) at Western Washington University. 
 
@@ -7,9 +7,54 @@ Analysis of facial recognition and verification systems on AI generated facial i
 
 ---
 
+## About
 
 Facial recognition is increasingly used as a trusted method to identify individuals. Facial recognition is used in many applications, including authentication for unlocking personal devices or in identifying individuals in digital images. Simultaneously, we have seen the quality of Artificial Intelligence systems increase and these systems become more widely available to the general public. 
 
 With the ability to recreate a person's face digitally, people could use AI generated images of real faces in an effort to create fraudulent media to tarnish a person's identity. People generally speculate about whether digital media being AI generated, but sometimes cannot determine with absolute certainty. 
 
 Given the multiple instances of ongoing or future privacy violations, our project scope is generalized to focus on the question of the susceptibility of facial recognition models against AI generated faces. We will look at face verification between a set of AI generated faces and real faces, and between a set of real faces and AI generated faces. In the first scenario, we will look to answer whether facial recognition models trained on synthetic face data neutralize privacy protections such as Fawkes. In the second scenario, we will answer whether AI generated facial images produce sufficiently similar embeddings to real faces to the point where they can pass verification against facial recognition systems.
+
+---
+
+## Disclaimer
+
+Our repo contains multiple directories with hundreds of images. Overall, this project will take up a lot of space and require even more in order to install dependencies. The python program will allow you to preform operations on the dataset, which were created by us during the process of creating these datasets. The functions are not currently grouped and require manual inspection in order to preform the expected operation. In other words, some functions are used for mass file operations and others are used for analysis with **DeepFace**.
+
+---
+
+## Project Code
+
+All code for the project is organized under the **src** directory. We used **python** as our langauge of choice, due to the great level of support for libraries that would be essential for this project. We used **uv** as our python package manager, and highly reccomend also using it when running this code.
+
+To run the python program, make sure you use the following command before to install dependices:
+
+```
+uv sync
+```
+
+Then to run the program use:
+
+```
+uv run python main.py
+```
+
+We recognize there is not a high requirement in order to run the code for our project. Most of the code we developed was directly used for processing and creating our datasets, and leftover as an artifact to satisfy our requirement for a built component. In our paper, we discuss this further in detail and reccomend future development go towards a user-centered application which will utilize the Fawkes tool creating a more accessible and usable interface.
+
+---
+
+## Project Artifacts
+
+The datasets we gathered and processed for our project is available on this directory. The final data analysis and processing for our results was done seperately on [Google Sheets](https://docs.google.com/spreadsheets/d/1N39Xokwnqrs3_SHyLGpE1A81fXSeJ4gRx293ymM1d7Y/edit?usp=sharing). 
+
+Since the naming of our directories may be confusing, we provided a guide to which images are in each directory and which step they represent in our research process.
+
+1. **images**: This is the original dataset we collected from the [UTXFace dataset](https://susanqq.github.io/UTKFace/). We cleaned the entries in this dataset to match the number of entries in every other dataset for simplicity. The images we removed were ones that were not successfully processed by DeepFace often due to no face being recognized. We ran into this issue later in our analysis, which we dive deeper into in our paper.
+2. **images-cloaked-low**, **images-cloaked-med**: These directories contain the images processed by fawkes at the low and medium settings. Fawkes allows three levels of processing (low, mid, high). We decided to not include high processing images as the processing time was excessive.
+3. **results**: This contains the final CSV file with the distance results from our final analysis. 
+
+---
+
+## Other Components
+
+Our project uses Fawkes for image cloaking. However, we only found this tool to be usable from downloading the binary off of their [official website](https://sandlab.cs.uchicago.edu/fawkes/). If you would like to recreate this experiment, we highly reccomend following that route and researching the CLI use of the tool.
--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+# Faices
+# CSCI-497
+# Charlie Laursen, Kyle Downing
+
 from deepface import DeepFace
 import os
 import random
@@ -6,52 +10,43 @@ import csv
 import requests
 import json
 import urllib.request
-# from fawkes.protection import Fawkes
 
 def swap_words(s, x, y):
+    """
+    Swap all occurnaces of two substrings within a given string
+
+    Args:
+        s: Input string
+        x: Substring
+        y: Substring
+
+    Returns:
+        New String
+    """
     return y.join(part.replace(y, x) for part in s.split(x))
+
 
 # DateTime for unique file naming. Add the Fawkes mode to the filename as well.
 DATE=swap_words(datetime.datetime.now().strftime("%x-%H:%M:%S"), "/", "_")
 
-
-
-
 def main():
-    # # Remove invalid faces not recognized by the model.
-    # # remove_invalid_faces()
-    # print(f'-----Program invoked at: {DATE}-----\n')
+    print(f'-----Program invoked at: {DATE}-----\n')
+    # Depending on your usage, invoke the corresponding function.
+    print(f'\n-----Program terminated at: {swap_words(datetime.datetime.now().strftime("%x-%H:%M:%S"), "/", "_")}-----')
 
-    # # Similarity Check
-    # #result = cloaked_similarity_check()
-    # #print('\n\nResult list:')
-    # #for r in result:
-    #     #print(r)
 
-    # content = [["1", "2"], ["a", "b"], ["c", "d"]]
-    # csv_writer(content)
-
-    # # TODO: Write Results to CSV File
-
-    # # TODO: Generate Synthetic Dataset
-
-    # print(f'\n-----Program terminated at: {swap_words(datetime.datetime.now().strftime("%x-%H:%M:%S"), "/", "_")}-----')
-    
-    # #used to move the images
-    # #move_cloaked_images("../images-0", "../images-cloaked-low")
-    # #move_cloaked_images("../images-1", "../images-cloaked-low")
-
-    #generate_ai_images("../images-cloaked-med")
-    #generate_ai_images("../images-ai-failed")
-    #missing_ai_images()
-    ret = face_check()
-    csv_writer(data=ret, filename_ext="final")
-
-def generate_synthetic_images():
-    pass
-
-# Verify all images return 100% similarities against themselves
 def similarity_check() -> bool:
+    """
+    Run similarity checks between all images against themselves,
+    ensuring those images can be processed by DeepFace and do not
+    cause unintended side effects later
+
+    Args:
+        None
+                
+    Returns:
+        Bool
+    """
     files = list(os.listdir('../images'))
     iteration = 0
     for f in files:
@@ -68,7 +63,7 @@ def similarity_check() -> bool:
     print('===== Finished =====')
     return True
 
-# return similarity between cloaked images and the original images
+
 def cloaked_similarity_check(cloaked_dir="../images_cloaked") -> []:
     """
     Run similarity check between cloaked images and normal images
@@ -102,32 +97,44 @@ def cloaked_similarity_check(cloaked_dir="../images_cloaked") -> []:
     return ret_list
 
 
-# IDK what to call this
 def face_check(source_dir="../images", cloaked_dir="../images-cloaked-med", ai_dir="../images-ai"):
+    """
+    Process face similarity checks between original and cloaked, and original and AI
+
+    Args:
+        source_dir: Directory containing original images
+        cloaked_dir: Directory containing cloaked images
+        ai_dir: Directory containing AI images
+
+    Returns:
+        List of results [filename, cloaked_distance, ai_distance]
+    """
     base_filenames = [f.split('.')[0] for f in list(os.listdir(source_dir))]
     cloaked_filenames = [f.split('.')[0] for f in list(os.listdir(cloaked_dir))]
     ai_filenames = [f.split('.')[0] for f in list(os.listdir(ai_dir))]
     for i in range(len(base_filenames)):
         assert base_filenames[i] in cloaked_filenames and base_filenames[i] in ai_filenames
-    print(base_filenames)
-    iteration = 0
+    total_files = len(base_filenames)
+    print(f'Total files to process: {total_files}')
     ret_list = []
-    ret_list.append(["filename", "verified", "distance"])
+    ret_list.append(["filename", "cloaked_distance", "ai_distance"])
+    # Process files
     for f in base_filenames:
         try:
-            print(f'===== Iteration [{iteration}/{len(base_filenames)}] - File {f} =====')
+            print(f'===== Iteration [{global_idx}/{total_files}] - File {f} =====')
             og_path = f'../images/{f}.jpg'
             cloaked_path = f'{cloaked_dir}/{f}.jpg'
             ai_path = f'../images-ai/{f}.jpg'
-            result = DeepFace.verify(img1_path=og_path, img2_path=cloaked_path)
-            baseline = DeepFace.verify(img1_path=og_path, img2_path=ai_path)
-            print(f'===== CLOAKED: {baseline["distance"]}, AI: {result["distance"]} =====\n')
-            iteration_result = [f, baseline["distance"], result["distance"]]
+            cloaked_result = DeepFace.verify(img1_path=og_path, img2_path=cloaked_path)
+            ai_result = DeepFace.verify(img1_path=og_path, img2_path=ai_path)
+            print(f'===== CLOAKED: {cloaked_result["distance"]}, AI: {ai_result["distance"]} =====\n')
+            iteration_result = [f, cloaked_result["distance"], ai_result["distance"]]
             ret_list.append(iteration_result)
         except Exception as e:
             print(f'Problem with DeepFace: {e.__str__()}')
-        iteration += 1
-    print('===== Finished =====')
+            # Add ERROR entry
+            ret_list.append([f, "ERROR", "ERROR"])
+    print('===== All batches finished =====')
     return ret_list
 
 
@@ -141,15 +148,12 @@ def remove_invalid_faces() -> None:
     Returns:
         None
     """
-    
     # Generate list of all files, random ordering
     files = list(os.listdir('../images'))
     random.shuffle(files)
     print(f'Total files: {len(files)}')
-    
     # Reference image for similarity check
     ref_img = "../images/100_0_0_20170112213500903.jpg"
-    
     # Run similarity check through each image
     iteration = 0
     deleted_count = 0
@@ -160,7 +164,6 @@ def remove_invalid_faces() -> None:
                 img1_path=ref_img,
                 img2_path=(f"../images/{f}")
             )
-            
         # If image is not valid, then remove from dataset
         except Exception as e:
             print(f'path: {f} not valid (Error: {type(e).__name__})... Attempting to delete file.')
@@ -175,10 +178,10 @@ def remove_invalid_faces() -> None:
     print(f'Total deleted: {deleted_count}')
 
 
-# For all images in the src directory, move them to the destination directory.
 def move_cloaked_images(src_path="../images", dest_path="../images-cloaked"):
     """
-    Move cloaked images from one directory to another
+    Move cloaked images from one directory to another. Defualt from images directory
+    where images are stored to the images-cloaked directory where they should exist.
 
     Args:
         src_path: Source directory path
@@ -213,11 +216,9 @@ def csv_writer(content: [[]], filename_ext="default", relative_dirpath="../resul
     Returns:
         None
     """
-    
     # Create directory if not exists
     os.makedirs(relative_dirpath, exist_ok=True)
     filepath = f'{relative_dirpath}results_{filename_ext}_{DATE}.csv'
-    
     # Write results to file
     print(f'Writing results to: {filepath}')
     with open(filepath, 'w', newline='') as file:
@@ -225,49 +226,56 @@ def csv_writer(content: [[]], filename_ext="default", relative_dirpath="../resul
         writer.writerows(content)
     print(f'Results successfully written to: {filepath}')
 
+
 def missing_ai_images():
+    """
+    Move AI images not in the processed AI diretory to the failed directory
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
     files = list(os.listdir('../images-cloaked-med'))
     iteration = 0
-    
     for f in files:
-            
         path = f'../images-ai/{f}'
-        
         if(os.path.exists(path) != True):
             print(path)
-        
             iteration += 1
-
             os.rename(f'{"../images-cloaked-med"}/{f}', f'{"../images-ai-failed"}/{f}')
-
     print(iteration)
-            
+    
 
 def generate_ai_images(input_dir): 
+    """
+    Generate a set of AI generated images
 
+    Args:
+        input_dir: filepath to input directory containing source images
+
+    Returns:
+        None
+    """
     images = list(os.listdir(input_dir))
-
     for f in images:
         try:
-
             path = f'{input_dir}/{f}'
-
             split_data = path.split(input_dir + "/")[1].split("_")
-            
-            if(split_data[1] == "0"):
+            if (split_data[1] == "0"):
                 split_data[1] = "male"
-            
             else:
                 split_data[1] = "female"
-
             files = [("input_image", open(path, "rb"))]
+            # Payload for API request
             payload = {
                 "text_prompt": "Marketer, " + split_data[1] + ", " + split_data[0] + " years old, professional headshot, solid background, portrait for linkedin",
                 "face_scale": 0.4,
                 "face_pos_x": 0.5,
                 "face_pos_y": 0.5,
             }
-
+            # API Request
             response = requests.post(
                 "https://api.gooey.ai/v2/FaceInpainting/form/",
                 headers={
@@ -276,24 +284,32 @@ def generate_ai_images(input_dir):
                 files=files,
                 data={"json": json.dumps(payload)},
             )
-            
             assert response.ok, response.content
-
             result = response.json()
             output_images = result.get('output', {}).get('output_images', [])
-
             urllib.request.urlretrieve(output_images[0], "../images-ai/" + path.split(input_dir)[1])
-            
         except Exception as e:
-            print("ERROR!", e)
+            print("ERROR!", e.__str__())
     
 
-def mass_rename():
-    #cloaked_filenames = [d.split('_mid')[0] for f in [f.split('.')[0] for f in list(os.listdir())]]
-    for f in list(os.listdir('../images-ai')):
-        new = f.split('.')[0].split('_mid')[0]
+
+def mass_rename(input_dir="../images-ai", cloaked_ext="_mid"):
+    """
+    Rewrite filepaths of images in a directory, given the cloaked filename extension.
+    This script should be used when fawkes generated a set of images and filenames are
+    modified from the original, but should be renamed back to the original.
+
+    Args:
+        input_dir: Input directory containing images with incorrect filepaths
+        cloaked_ext: Filename extension given by fawkes after processing
+
+    Returns:
+        None
+    """
+    for f in list(os.listdir(input_dir)):
+        new = f.split('.')[0].split(cloaked_ext)[0]
         os.rename(f'../images-ai/{f}', f'../images-ai/{new}.jpg')
 
+
 if __name__ == "__main__":
-    mass_rename()
     main()
